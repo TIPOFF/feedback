cd1b263af1d356d2dfb236565c12349b92163224
Chris H <crishack8@gmail.com>
2021-02-04 16:16:41 -0600

    Add Feedback model & Feedback Factory.

---
+++ src/Models/Feedback.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tipoff\Feedback\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Tipoff\Support\Models\BaseModel;
+
+class Feedback extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $guarded = ['id'];
+
+    protected $casts = [
+        'emailed_at' => 'datetime',
+        'opened_at' => 'datetime',
+        'clicked_negative_at' => 'datetime',
+        'clicked_semi_negative_at' => 'datetime',
+        'clicked_semi_positive_at' => 'datetime',
+        'clicked_positive_at' => 'datetime',
+        'redirected_at' => 'datetime',
+        'clicked_review_at' => 'datetime',
+        'submitted_at' => 'datetime',
+        'date' => 'date',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($feedback) {
+            if (empty($feedback->token)) {
+                $feedback->token = $feedback->location_id . 'L' . mt_rand(1000, 9999) . 'TGER' . mt_rand(1, 999999);
+            }
+        });
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'token';
+    }
+
+    public function participant()
+    {
+        return $this->belongsTo(config('waivers.model_class.participant'), 'participant_id');
+    }
+
+    public function location()
+    {
+        return $this->belongsTo(config('waivers.model_class.location'), 'location_id');
+    }
+}
