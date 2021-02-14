19093eb985ac5e4dbdc700ef3ecd128dcc13d11c
chx2 <chx2@users.noreply.github.com>
2021-02-14 01:42:30 +0000

    Fix styling

--- src/Nova/Feedback.php
+++ src/Nova/Feedback.php
@@ -158,7 +158,6 @@ class Feedback extends Resource
 
     public function filters(Request $request)
     {
-
     }
 
     public function lenses(Request $request)
--- tests/Feature/Nova/FeedbackResourceTest.php
+++ tests/Feature/Nova/FeedbackResourceTest.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 namespace Tipoff\Feedback\Tests\Feature\Nova;
 
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-use Tipoff\TestSupport\Models\User;
 use Tipoff\Feedback\Models\Feedback;
 use Tipoff\Feedback\Tests\TestCase;
+use Tipoff\TestSupport\Models\User;
 
 class FeedbackResourceTest extends TestCase
 {
